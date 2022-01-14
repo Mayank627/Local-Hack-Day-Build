485a8ab73321b66231f8a5e7acb25469b2ccf32c
Mayank <77638235+Mayank627@users.noreply.github.com>
2022-01-14 13:54:07 +0530

    Add files via upload

---
+++ SortAList.py
@@ -0,0 +1,12 @@
+list = [30, 20, 10, 50, 40]
+  
+# Sorting Integers in ascending Order
+#list.sort()
+
+#print(list)
+
+#Sorting Integers in Descending Order
+
+list.sort(reverse=True)
+print("The sorted list is:")
+print(list)
